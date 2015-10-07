9e8a0c7ace7f98c5b08b30f422d68c4b9aa75f81
Jaromir Sedlacek <sedlacek@avast.com>
2015-10-07 16:52:55 +0200

    Adding file parameter and artifacts downloads

--- build.py
+++ build.py
@@ -3,12 +3,78 @@ __author__ = 'sedlacek'
 from jenkinsapi.jenkins import Jenkins
 from jenkinsapi.jenkinsjob import JenkinsJob
 from jenkinsapi.requester import JenkinsAuth
+import jenkinsapi.jenkinsartifacts
+
+import re
+
 import logging
 
+logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 import argparse
 from ast import literal_eval
+
+
+class OpenFiles(object):
+    """
+    Context manager opening all files and making sure that in the end all are closed ...
+    """
+    def __init__(self, files, mode='rb'):
+        """
+        :param dict files:      dict {paramname: filename}
+        :param str mode:        mode, default 'rb'
+        :return dict:           dictionary containing open files  {paramname: file}
+        """
+        self._files = dict(files)
+        self._mode = mode
+        self._openfiles = {}
+
+    def __enter__(self):
+        """
+        Open all files
+        :return dict:           dictionary containing open files  {paramname: file}
+        """
+        self._openfiles = {key: open(value, self._mode) for key, value in self._files.iteritems()}
+        # intentionally making a copy - we do not want having some files accidentally removed from dict
+        return dict(self._openfiles)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """
+        close all files
+        """
+        for openfile in self._openfiles.itervalues():
+            try:
+                openfile.close()
+            except Exception:
+                pass
+        # make sure exceptions are handled upstream
+        return False
+
+
+class RegExFilter(object):
+    """
+    Callable for filtering out unwanted atrifacts
+    """
+    def __init__(self, regex):
+        """
+        :param regex:   regular expression
+        """
+        try:
+            self._regex = re.compile(regex)
+        except Exception as e:
+            # broken regex
+            e.message += '\nInvalid Regular Expression'
+            raise e
+
+    def __call__(self, string):
+        """
+        if string match regex return true
+
+        :param str|unicode string:      string to be matched
+        :return re.MatchObject|bool:
+        """
+        return self._regex.search(string)
 
 parser = argparse.ArgumentParser(description='Build a Job')
 parser.add_argument('--jenkins', required=True, metavar='<jenkins>', help='jenkins url')
@@ -21,7 +87,8 @@ parser.add_argument('--noblock', action='store_true', help='do not wait until bu
 parser.add_argument('--noconsole', action='store_true', help='do no copy console output to stdout')
 parser.add_argument('--cause', required=False, default=None,  metavar='<cause>', help='build cause')
 parser.add_argument('--level', required=False, default='WARNING',  metavar='<debug level>', help='Debug Level')
-parser.add_argument('params', metavar='param1=value', nargs='*', help='build parameters, file type paramenter is not supported yet')
+parser.add_argument('--artifacts', required=False, default=None, metavar='<artifacts>', help='Artifacts to download (regex) or ALL')
+parser.add_argument('params', metavar='param1=value', nargs='*', help='build parameters, file type param2=@filename')
 
 
 args = vars(parser.parse_args())
@@ -32,9 +99,13 @@ jenkins = Jenkins(url=args['jenkins'], auth=JenkinsAuth(username=args['user'], p
 job = JenkinsJob(parent=jenkins, objid=args['job'])
 
 params = {}
+files = {}
 for param in args['params']:
     key, value = param.split('=', 1)
-    params[key] = value
+    if value.startswith('@'):
+        files[key] = value[1:]
+    else:
+        params[key] = value
 if len(params) == 0:
     params = None
 
@@ -45,14 +116,18 @@ if console:
     # we have to wait for job to finish
     block = True
 
-qitem = job.enqueue_build(cause=args['cause'], params=params)
-logger.info('Job enqueued as queue item %s' % qitem.url)
+if files != {}:
+    with OpenFiles(files, mode='rb') as openfiles:
+        qitem = job.enqueue_build(cause=args['cause'], params=params, files=openfiles)
+else:
+    qitem = job.enqueue_build(cause=args['cause'], params=params)
+logger.info(' Job enqueued as queue item %s' % qitem.url)
 
 if block:
     # wait for build to be dequeued
-    logger.debug('Waiting for build to be dequeued')
+    logger.debug(' Waiting for build to be dequeued')
     qitem.block()
-    logger.debug('Build dequeued.')
+    logger.debug(' Build dequeued.')
 else:
     # we do not wait for anything, so exit in any case
     exit(0)
@@ -62,7 +137,7 @@ build = qitem.build
 if build is not None:
     build.poll()
 else:
-    logger.error('Build perhaps cancelled while has been in the queue.')
+    logger.error(' Build perhaps cancelled while has been in the queue.')
     # queue item has been probably cancelled :(
     exit(1)
 
@@ -74,11 +149,20 @@ if console:
         'Something wrong happened, build is still building, ' \
         'but console polling does not have more data :('
 else:
-    logger.debug('Waiting for build to finish')
+    logger.debug(' Waiting for build to finish')
     build.block()
 
+if args['artifacts'] is not None:
+    # download artifacts
+    if args['artifacts'] == 'ALL':
+        _artifacts = None
+    else:
+        _artifacts = RegExFilter(args['artifacts'])
+
+    build.artifacts.writeall(filter_artifacts=_artifacts)
+
 if not build.ok:
-    logger.error('Build finished with status %s' % build['result'])
+    logger.error(' Build finished with status %s' % build['result'])
     exit(10)
 
-logger.info('Build finished with status %s' % build['result'])
+logger.info(' Build finished with status %s' % build['result'])
